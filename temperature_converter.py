26cbe10b27f8a9c659cdf06e74320fe28f30bd3d
Rita <catshre@gmail.com>
2024-04-23 16:48:31 +0200

    change port and host

--- temperature_converter.py
+++ temperature_converter.py
@@ -7,4 +7,4 @@ def hello_geek():
     return '<h1>Welcome to the Temperature Converter</h2>'
 
 if __name__ == '__main__':
-    app.run(host="127.0.0.1", port=8080, debug=True)
+    app.run(host="0.0.0.0", debug=True)
